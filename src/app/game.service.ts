0e72536504e8b07f4709a89aaed95201864ecb81
Yonatan Lilling <yonatanli@c4internal.c4-security.com>
2018-05-03 10:30:11 +0300

    fix win option

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -7,7 +7,7 @@ export class GameService {
   winningOptions = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [3, 4, 7]
+    [0, 4, 8], [3, 4, 6]
   ];
   cells: Cell[];
   turnsNumber: number;
